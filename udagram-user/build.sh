b8a3178c5a98e5ab9e5ff406c9ad0e0fb656fd16
Jascha Scholz <jascha_scholz@web.de>
2021-05-17 23:36:06 +0200

    adds dockerfile to udagram-user

---
+++ udagram-user/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+docker build \
+--build-arg username=$POSTGRES_USERNAME \
+--build-arg password=$POSTGRES_PASSWORD \
+--build-arg database=$POSTGRES_DB \
+--build-arg host=$POSTGRES_HOST \
+--build-arg aws_region=$S3_REGION \
+--build-arg aws_profile=$AWS_PROFILE \
+--build-arg aws_media_bucket=$S3_BUCKET \
+.
