8fcb3d2d7c97f4a60e1e591b9c95d25838ca35e0
Jascha Scholz <jascha_scholz@web.de>
2021-05-18 21:17:50 +0200

    adds udagram user docker build to travis ci files

--- travis_script.sh
+++ travis_script.sh
@@ -10,3 +10,17 @@ docker build -t $1/udagram:feed \
 --build-arg aws_profile=$7 \
 --build-arg aws_media_bucket=$8 \
 .
+
+cd ..
+
+cd udagram-user
+
+docker build -t $1/udagram:user \
+--build-arg username=$2 \
+--build-arg password=$3 \
+--build-arg database=$4 \
+--build-arg host=$5 \
+--build-arg aws_region=$6 \
+--build-arg aws_profile=$7 \
+--build-arg aws_media_bucket=$8 \
+.
